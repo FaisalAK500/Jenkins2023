8ee38df863b23e29997633f0c1a4cb83a31f66bc
Faisal <faisalahmedk15@gmail.com>
2023-02-21 08:41:55 +0530

    Files added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World Through CMD Command:");
+		System.out.println("India is My Country");
+		System.out.println("Faisal Ahmed Khan");
+		System.out.println("Sharif Khan");
 		
 	}
 
