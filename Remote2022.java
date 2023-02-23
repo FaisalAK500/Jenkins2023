64d057093f248d5ab16678da8eb2b5dbe329124a
Faisal <faisalahmedk15@gmail.com>
2023-02-23 07:23:52 +0530

    Remote Files Added

---
+++ Remote2022.java
@@ -0,0 +1,11 @@
+public class Remote2022  {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is the remote job running code:");
+		System.out.println("This is the remote job running code in 2022:");
+		
+		
+	}
+
+}
